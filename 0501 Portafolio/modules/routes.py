795f8c3d2edfcfd07c75eb1acd74f86c0bd5cc57
datajm <ecojmbarrera@gmail.com>
2024-06-17 22:26:13 -0600

    documentacion clase 05

--- 0501 Portafolio/modules/routes.py
+++ 0501 Portafolio/modules/routes.py
@@ -1,30 +1,46 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 
+# Crear un blueprint llamado 'router'
 router = Blueprint('router', __name__)
 
 @router.route('/')
 def home():
-    return  render_template ('index.html')
+    """
+    Ruta para la página de inicio.
+    """
+    return render_template('index.html')
 
 @router.route('/proyectos')
 def proyectos():
-    return  render_template ('proyectos.html')
+    """
+    Ruta para la página de proyectos.
+    """
+    return render_template('proyectos.html')
 
 @router.route('/about')
 def about():
-    return  render_template ('about.html')
+    """
+    Ruta para la página 'About me'.
+    """
+    return render_template('about.html')
 
-@router.route('/comentarios', methods=['GET','POST'])
+@router.route('/comentarios', methods=['GET', 'POST'])
 def comentarios():
-    if request.method=='POST':
-        
-        # TODO: Cuidar la seguridad (es confiable la información que viene en el comentario?)
+    """
+    Ruta para la página de comentarios.
+    Permite tanto solicitudes GET como POST.
+    """
+    if request.method == 'POST':
         comentario = request.form.get('comentario')
 
-        # TODO: Procesar el comentario
+        # Procesar el comentario
         print(f'Recibí el siguiente comentario: {comentario}')
 
-        flash('Hemos recibido tu comentario, muchas gracias !!!')
+        # Mostrar un mensaje flash para informar al usuario que su comentario fue recibido
+        flash('Hemos recibido tu comentario, muchas gracias !!!', 'success')
+
+        # Redirigir a la misma página de comentarios
         return redirect(url_for('router.comentarios'))
 
-    return  render_template ('comentarios.html')
+    # Si el método de la solicitud es GET, renderizar la plantilla de comentarios
+    return render_template('comentarios.html')
