930d98af449523eada13a31f5e2abfca89f50b95
datajm <ecojmbarrera@gmail.com>
2024-06-27 10:45:48 -0600

    avances 3 ejemplo final

--- 1000 flask_shopping_list template/app/main/routes.py
+++ 1000 flask_shopping_list template/app/main/routes.py
@@ -17,6 +17,14 @@ SessionLocal = create_sessionLocal(SQLALCHEMY_DATABASE_URI)
 #     Returns:
 #         function: La función decorada.
 #     """
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'logged_in' not in session:
+            # Redirige al usuario al login si no está autenticado
+            return redirect(url_for('main.index'))
+        return f(*args, **kwargs)
+    return decorated_function
 
 # @main.route('/')
 # @main.route('/index')
@@ -28,6 +36,13 @@ SessionLocal = create_sessionLocal(SQLALCHEMY_DATABASE_URI)
 #     Returns:
 #         str: Redirige a la lista principal en caso de estar autenticado o renderiza la plantilla de inicio.
 #     """
+@main.route('/')
+@main.route('/index')
+def index():
+    if 'logged_in' in session:
+        return redirect(url_for('main.list'))
+    else:
+        return render_template('main/index.html')
 
 # @main.route('/list', methods=['GET', 'POST'])
 # def list():
@@ -38,6 +53,32 @@ SessionLocal = create_sessionLocal(SQLALCHEMY_DATABASE_URI)
 #     Returns:
 #         str: Renderiza la plantilla de listas en caso de GET o redirige a la lista principal en caso de éxito.
 #     """
+@main.route('/list', methods=['GET', 'POST'])
+@login_required
+def list():
+    if request.method == 'POST':
+        # Esta es la lógica para guardar listas
+        list_name = request.form.get('list_name')
+        items = request.form.get('items')
+        new_list = ShoppingList(name=list_name, items=items, user_id=session["current_user_id"])
+
+        session_local = SessionLocal()
+        try:
+            session_local.add(new_list)
+            session_local.commit()
+            session_local.refresh(new_list)
+            flash('List created successfully!', 'success')
+        except:
+            session_local.rollback()
+            flash('Ocurrió un error al guardar', 'danger')
+        finally:
+            session_local.close()
+        return redirect(url_for('main.list'))
+
+    session_local = SessionLocal()
+    user_lists = session_local.query(ShoppingList).filter_by(user_id=session["current_user_id"]).all()
+    session_local.close()
+    return render_template('main/list.html', user_lists=user_lists)
 
 # @main.route('/list/<list_id>/delete', methods=['POST'])
 # def delete_list(list_id):
@@ -50,3 +91,22 @@ SessionLocal = create_sessionLocal(SQLALCHEMY_DATABASE_URI)
 #     Returns:
 #         Redirige a la lista principal después de eliminar la lista.
 #     """
+@main.route('/list/<list_id>/delete', methods=['POST'])
+@login_required
+def delete_list(list_id):
+    session_local = SessionLocal()
+    list_to_delete = session_local.query(ShoppingList).filter(ShoppingList.id==list_id).first()
+
+    if list_to_delete.user_id != session["current_user_id"]:
+        abort(403)
+
+    try:
+        session_local.delete(list_to_delete)
+        session_local.commit()
+        flash('List deleted successfully!', 'success')
+    except:
+        session_local.rollback()
+        flash('Ocurrió un error al borrar', 'danger')
+    finally:
+        session_local.close()
+    return redirect(url_for('main.list'))
